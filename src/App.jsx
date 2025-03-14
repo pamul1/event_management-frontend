4e4e964b3bd1e7fea14d91d618b807ee18983290
pamul1 <109163253+pamul1@users.noreply.github.com>
2025-03-13 21:18:02 -0500

    Commitlast1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LogIn } from './screenComponents/LogIn'
 import { NotFound } from './components/NotFound'
@@ -9,15 +9,46 @@ import { EventScreen } from './screenComponents/EventScreen'
 
 export const App = () => {
 
+  const [isLogIn , setIsLogIn] = useState(false)
+
+  let token = ""
+  const baseUrl = import.meta.env.VITE_BASE_URL
+  const endPoint = "validateSesion"
+
+  const validateToken = async()=>{
+      const newUrl = `${baseUrl}${endPoint}`
+      const result = await fetch(newUrl, {
+        method: "POST", 
+        headers: {
+            'Authorization' : token,
+            'Content-Type' : 'application/json'
+        }
+      })
+
+      if (result.ok) {
+        setIsLogIn(true)
+        window.location.href = '/event'
+      }else {
+        setIsLogIn(false)
+      }
+
+  }
+
+  useEffect(()=>{
+
+    token = window.localStorage.getItem('event-credential')
+    validateToken()
+
+  }, [])
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<LogIn />}/>
-          <Route path='/register' element={ <Register/>}/>
+          {isLogIn ?<Route path='/register' element={ <Register/>}/>: ""}
           <Route path='*' element={<NotFound />} />
-          <Route path='/attendance/:id_event' element={ <AttendanceScreen/>}/>
-          <Route path='/event' element={ <EventScreen/>}/>
+          {isLogIn ?<Route path='/attendance/:id_event' element={ <AttendanceScreen/>}/>: ""}
+          {isLogIn ?<Route path='/event' element={ <EventScreen/>}/>: ""}
 
         </Routes>
       </BrowserRouter>

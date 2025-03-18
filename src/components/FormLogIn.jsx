6226fee9cc64b6ac2b99e73ec5c22a8af7c96dbe
frlopez88 <frlopez88@icloud.com>
2025-03-17 20:00:06 -0600

    last try with redirection

--- src/components/FormLogIn.jsx
+++ src/components/FormLogIn.jsx
@@ -45,7 +45,7 @@ export const LogInForm = () => {
             const email = formData.email
             window.localStorage.setItem("event-credential", token)
             window.localStorage.setItem("event-email", email)
-            navigate("/event")
+            window.location.href = "#/event"
             
         }else{
             console.log("Invalid Credential")
